37f42578bbd6f6c7f1a2e45d991624438c1dc4fa
Grant Heffernan <heffergm@gmail.com>
2015-03-18 14:55:48 -0400

    lint

--- jobs/pelias.rb
+++ jobs/pelias.rb
@@ -3,7 +3,19 @@ require 'json'
 es_endpoint = "http://localhost:9200/pelias"
 
 # counts
-%w(geoname openaddresses osmnode osmway osmaddress admin0 admin1 admin2 local_admin locality neighborhood).each do |t|
+%w(
+  geoname
+  openaddresses
+  osmnode
+  osmway
+  osmaddress
+  admin0
+  admin1
+  admin2
+  local_admin
+  locality
+  neighborhood
+).each do |t|
   SCHEDULER.every '10s' do
     url = URI.parse "#{es_endpoint}/#{t}/_count"
     response = JSON::parse Net::HTTP.get_response(url).body
