e26f3cf3a190fa414ff5a5c842069b76392ec4d1
Grant Heffernan <heffergm@gmail.com>
2015-03-19 12:29:31 -0400

    add indexing time and doc count

--- jobs/pelias.rb
+++ jobs/pelias.rb
@@ -27,16 +27,19 @@ es_endpoint = ENV['ES_ENDPOINT'] || "http://localhost:9200/pelias"
 end
 
 # es metrics
-SCHEDULER.every '1m' do
-  url = URI.parse "#{es_endpoint}/_stats/store?human"
+SCHEDULER.every '10s' do
+  url = URI.parse "#{es_endpoint}/_stats?human"
   response = JSON::parse Net::HTTP.get_response(url).body
+
   store_size = response['indices']['pelias']['primaries']['store']['size']
   send_event('es-store-size', { text: store_size })
-end
+  
+  index_time = response['indices']['pelias']['primaries']['indexing']['index_time']
+  send_event('es-index-time', { text: index_time })
 
-SCHEDULER.every '1m' do
-  url = URI.parse "#{es_endpoint}/_stats/completion?human"
-  response = JSON::parse Net::HTTP.get_response(url).body
+  doc_count  = response['indices']['pelias']['primaries']['docs']['count']
+  send_event('es-doc-count', { current: doc_count })
+
   completion_size = response['indices']['pelias']['primaries']['completion']['size']
   send_event('es-completion-size', { text: completion_size })
 end
