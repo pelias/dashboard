31bddc236291685f45432723508bcbae689bf09e
Grant Heffernan <heffergm@gmail.com>
2015-05-19 18:46:51 -0400

    expected % complete

--- jobs/es_metrics.rb
+++ jobs/es_metrics.rb
@@ -3,6 +3,21 @@ require 'json'
 # Allow specification of an elasticsearch endpoint vian env var.
 #   Should take the form of "http://{ip|hostname}:{port}/{index}"
 es_endpoint = ENV['ES_ENDPOINT'] || 'http://localhost:9200/pelias'
+expected_doc_count = ENV['EXPECTED_DOC_COUNT'] || nil
+
+# percent complete
+unless expected_doc_count.nil?
+  SCHEDULER.every '10s' do
+    url = URI.parse "#{es_endpoint}/_stats/indexing?human"
+    response = JSON.parse Net::HTTP.get_response(url).body
+    indexed = response['indices']['pelias']['primaries']['indexing']['index_total']
+
+    percent_complete = ((indexed.to_f / expected_doc_count.to_f) * 100).to_i
+    percent_complete > 100 ? percent_complete = 100 : percent_complete
+
+    send_event('percent-complete', text: "#{percent_complete}%")
+  end
+end
 
 # es metrics
 SCHEDULER.every '1m' do
